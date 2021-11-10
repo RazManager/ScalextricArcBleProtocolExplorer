cd7ce9f413176a01005b4da9af5f2b2a33b1d3fe
Razamanaz <anders@razlogic.se>
2021-11-10 21:48:04 +0100

    ScalextricArcBleProtocolExplorer.csproj created

---
+++ dotnet/ScalextricArcBleProtocolExplorer/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80);
+});
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
