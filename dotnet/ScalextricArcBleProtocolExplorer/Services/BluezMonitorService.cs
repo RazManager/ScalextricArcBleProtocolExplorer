d45eb0160a819980e0e76f728f69adb68cd3e6a4
RazManager <razmanager@gmail.com>
2021-11-11 20:22:49 +0100

    Bluez testing

--- dotnet/ScalextricArcBleProtocolExplorer/Services/BluezMonitorService.cs
+++ dotnet/ScalextricArcBleProtocolExplorer/Services/BluezMonitorService.cs
@@ -11,6 +11,36 @@ namespace ScalextricArcBleProtocolExplorer.Services
     {
         private readonly ILogger<BluezMonitorService> _logger;
 
+        public const string serviceName = "org.bluez";
+        public const string AdapterInterface = "org.bluez.Adapter1";
+        public const string DeviceInterface = "org.bluez.Device1";
+        public const string GattServiceInterface = "org.bluez.GattService1";
+        public const string GattCharacteristicInterface = "org.bluez.GattCharacteristic1";
+
+        //[DBusInterface("org.freedesktop.DBus.ObjectManager")]
+        //interface IObjectManager : IDBusObject
+
+        //[DBusInterface("org.bluez.AgentManager1")]
+        //interface IAgentManager1 : IDBusObject
+
+        //[DBusInterface("org.bluez.ProfileManager1")]
+        //interface IProfileManager1 : IDBusObject
+
+        //[DBusInterface("org.bluez.Adapter1")]
+        //interface IAdapter1 : IDBusObject
+
+        //[DBusInterface("org.bluez.GattManager1")]
+        //interface IGattManager1 : IDBusObject
+
+        //[DBusInterface("org.bluez.LEAdvertisingManager1")]
+        //interface ILEAdvertisingManager1 : IDBusObject
+
+        //[DBusInterface("org.bluez.Media1")]
+        //interface IMedia1 : IDBusObject
+
+        //[DBusInterface("org.bluez.NetworkServer1")]
+        //interface INetworkServer1 : IDBusObject
+
 
         public BluezMonitorService(ILogger<BluezMonitorService> logger)
         {
@@ -24,6 +54,8 @@ namespace ScalextricArcBleProtocolExplorer.Services
             {
                 try
                 {
+                    Console.WriteLine(Environment.OSVersion.Platform);
+
                     var services = await Tmds.DBus.Connection.System.ListServicesAsync();
                     Console.WriteLine($"Number of services: {services.Length}");
                     foreach (var service in services)
@@ -37,6 +69,10 @@ namespace ScalextricArcBleProtocolExplorer.Services
                     {
                         Console.WriteLine(activatableServices);
                     }
+
+                    var x = Tmds.DBus.Connection.System.CreateProxy<bluez.DBus.IObjectManager>(serviceName, GetType(bluez.DBus.IObjectManager.   "d");
+                    x.
+
 
                     _logger.LogInformation("BluezMonitorService is running...");
                     await Task.Delay(10000, cancellationToken);
