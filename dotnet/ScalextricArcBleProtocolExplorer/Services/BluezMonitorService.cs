4e375456dd6ef37296d5e04db1f75f591693b689
RazManager <razmanager@gmail.com>
2021-11-11 23:04:55 +0100

    Testing ObjectManager

--- dotnet/ScalextricArcBleProtocolExplorer/Services/BluezMonitorService.cs
+++ dotnet/ScalextricArcBleProtocolExplorer/Services/BluezMonitorService.cs
@@ -3,7 +3,8 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ScalextricArcBleProtocolExplorer.Services
 {
@@ -56,26 +57,88 @@ namespace ScalextricArcBleProtocolExplorer.Services
                 {
                     Console.WriteLine(Environment.OSVersion.Platform);
 
-                    var services = await Tmds.DBus.Connection.System.ListServicesAsync();
-                    Console.WriteLine($"Number of services: {services.Length}");
-                    foreach (var service in services)
-                    {
-                        Console.WriteLine(service);
-                    }
+                    //var services = await Tmds.DBus.Connection.System.ListServicesAsync();
+                    //Console.WriteLine($"Number of services: {services.Length}");
+                    //foreach (var service in services)
+                    //{
+                    //    Console.WriteLine(service);
+                    //}
 
                     var activatableServices = await Tmds.DBus.Connection.System.ListActivatableServicesAsync();
-                    Console.WriteLine($"Number of activatable services: {activatableServices.Length}");
-                    foreach (var activatableService in activatableServices)
+                    //Console.WriteLine($"Number of activatable services: {activatableServices.Length}");
+                    //foreach (var activatableService in activatableServices)
+                    //{
+                    //    Console.WriteLine(activatableService);
+                    //}
+
+                    if (activatableServices.SingleOrDefault(x => x == serviceName) is null)
                     {
-                        Console.WriteLine(activatableServices);
+                        _logger.LogError($"{serviceName} is not an \"activateable\" D-Bus service. Please install bluez for the needed Bluetooth Low Energy functionality.");
+                        return;
                     }
 
-                    var x = Tmds.DBus.Connection.System.CreateProxy<bluez.DBus.IObjectManager>(serviceName, GetType(bluez.DBus.IObjectManager.   "d");
-                    x.
+                    var isServiceActive = await Tmds.DBus.Connection.System.IsServiceActiveAsync(serviceName);
+                    if (!isServiceActive)
+                    {
+                        _logger.LogInformation($"Starting {serviceName}.");
+                        var serviceStartResult = await Tmds.DBus.Connection.System.ActivateServiceAsync(serviceName);
+                        switch (serviceStartResult)
+                        {
+                            case Tmds.DBus.ServiceStartResult.Started:
+                                _logger.LogInformation($"{serviceName} started.");
+                                break;
+                            case Tmds.DBus.ServiceStartResult.AlreadyRunning:
+                                _logger.LogInformation($"{serviceName} was already running.");
+                                break;
+                        }
+                    }
+
+                    var objectManager = Tmds.DBus.Connection.System.CreateProxy<bluez.DBus.IObjectManager>(serviceName, "/");
+
+                    var watchInterfacesAddedTask = objectManager.WatchInterfacesAddedAsync(
+                        ((Tmds.DBus.ObjectPath objectPath, IDictionary<string, IDictionary<string, object>> interfaces) handler) =>
+                            {
+                                _logger.LogInformation($"{handler.objectPath} added...");
+                            },
+                        exception =>
+                        {
+                            _logger.LogError(exception.Message);
+                        }
+                    );
+
+                    var watchInterfacesRemovedTask = objectManager.WatchInterfacesRemovedAsync(
+                        ((Tmds.DBus.ObjectPath objectPath, string[] interfaces) handler) =>
+                        {
+                            _logger.LogInformation($"{handler.objectPath} removed...");
+                        },
+                        exception =>
+                        {
+                            _logger.LogError(exception.Message);
+                        }
+                    );
+
+                    var objectPaths = await objectManager.GetManagedObjectsAsync();
+                    Console.WriteLine($"{objectPaths.Count} objectPaths found.");
+                    foreach (var objectPath in objectPaths)
+                    {
+                        Console.WriteLine($"objectPath.Key={objectPath.Key}");
+                        foreach (var iface in objectPath.Value)
+                        {
+                            Console.WriteLine($"iface.Key={iface.Key}");
+                            foreach (var item in iface.Value)
+                            {
+                                Console.WriteLine($"item.Key={item.Key} item.Value={item.Value}");
+                            }
+                        }
+                    }
 
 
                     _logger.LogInformation("BluezMonitorService is running...");
                     await Task.Delay(10000, cancellationToken);
+
+                    watchInterfacesAddedTask.Dispose();
+                    watchInterfacesRemovedTask.Dispose();
+
                 }
                 catch (Exception exception)
                 {
